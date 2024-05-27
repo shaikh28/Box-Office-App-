076fa43b07911a6cda1868a1cdc2f43066fffb9a
Amaan's Git <shaikhhunt7262@gamil.com>
2024-05-27 14:46:53 +0530

    Installed React-Router-Dom And made Starred page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Starred from "./pages/Starred";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<div>Sorry You are at Wrong place</div>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Starred" element={<Starred />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Home = () => {
+  return (
+    <div>Home
+        <Link to="/Starred"> Starred</Link>
+    </div>
+  )
+}
+
+export default Home
