84f1adef588172d179dfb25f305e14741928ab6e
Amaan's Git <shaikhhunt7262@gamil.com>
2024-05-28 15:22:36 +0530

    added search button to search movies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import React from "react";
-import {useState} from 'react'
+import { useState } from "react";
 import { Link } from "react-router-dom";
 const Home = () => {
-  const [inputValue, setInputValue] = useState('')
+  const [searchStr, setSearchStr] = useState("");
 
-  console.log(inputValue);
-  const onInputChange = (ev)=>{
-    setInputValue(ev.target.value)
+  console.log(setSearchStr);
+  const onSearchInputChange = (ev) => {
+    setSearchStr(ev.target.value);
+  };
+
+  const onSearch = async (ev) => {
+    ev.preventDefault()
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+    const body = await response.json()
+      console.log(body);
+
+    // https://api.tvmaze.com/search/shows?q=boys
   }
-  return <div>
-    <div>{inputValue}</div>
-    <button onClick={()=>{setInputValue("Hunter")}}>updateRandom</button>
-    <input type="text" value={inputValue}  onChange={onInputChange} />
-  </div>;
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input name='Search'type="text" value={searchStr} onChange={onSearchInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
