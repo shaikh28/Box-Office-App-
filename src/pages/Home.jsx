0a9e47d08eb767c87be0318c919bca52871c78a5
Amaan's Git <shaikhhunt7262@gamil.com>
2024-06-01 15:05:47 +0530

    Used useQuery on home page to fecth search data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,42 @@
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { searchForPeoples, searchForShows } from "../api/TvMaze";
 import SearchForm from "../components/SearchForm";
 import ShowsGrid from "../components/shows/ShowsGrid";
 import ActorsGrid from "../components/actors/ActorsGrid";
 const Home = () => {
-  const [apiData, setApiData] = useState([]);
-  const [apiDataError, setApiDataError] = useState(null);
+
+  const [filter, setFilter] = useState("");
+  const { data: apiData, error: apiDataError } = useQuery({
+    queryKey: ["search", filter],
+    queryFn: () =>
+      filter.searchOption === "shows"
+        ? searchForShows(filter.q)
+        : searchForPeoples(filter.q),
+
+    enabled: !!filter,
+    refetchOnWindowFocus:false
+  });
 
   const onSearch = async ({ q, searchOption }) => {
-    try {
-      setApiDataError(null);
-      let results;
-      if (searchOption === "shows") {
-        results = await searchForShows(q);
-        setApiData(results);
-      } else {
-        results = await searchForPeoples(q);
-      }
-      setApiData(results);
-    } catch (error) {
-      setApiDataError(error);
-    }
+    setFilter({ q, searchOption });
   };
   const renderApiData = () => {
     if (apiDataError) {
       return <div>Something Went Wrong{apiDataError.message}</div>;
     }
-    if(apiData?.length === 0){
-      return <div>No Results Found</div>
+    if (apiData?.length === 0) {
+      return <div>No Results Found</div>;
     }
-  
+
     if (apiData) {
       if (apiData && apiData.length > 0) {
-        return apiData[0].show ? <ShowsGrid shows={apiData} /> : <ActorsGrid actors={apiData}/>;
+        return apiData[0].show ? (
+          <ShowsGrid shows={apiData} />
+        ) : (
+          <ActorsGrid actors={apiData} />
+        );
         // apiData.map((data) => (
         //     <div key={data.show.id}>{data.show.name}</div>
         //   ))
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/TvMaze";
 const Show = () => {
   const { showId } = useParams();
- const {data : showData , error : showError} = useQuery({  
-    queryKey:['show',showId],
-    queryFn: () =>getShowById(showId)
-  })
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ["show", showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>Something went Wrong {showError.message}</div>;
