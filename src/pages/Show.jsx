4c8fc75eabc1c664ce6067ee10f00613ec833958
Amaan's Git <shaikhhunt7262@gamil.com>
2024-06-01 14:06:13 +0530

    Added custom hooks in show.jsx

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -2,31 +2,32 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/TvMaze";
 
-const Show = () => {
-  const { showId } = useParams();
+const useShowbyId = (showId) => {
   const [showData, setShowData] = useState(null);
   const [showError, setShowError] = useState(null);
   useEffect(() => {
     async function fecthData() {
       try {
         const data = await getShowById(showId);
-        setShowData(data)
+        setShowData(data);
         console.log(data);
       } catch (err) {
-        setShowError(err)
+        setShowError(err);
       }
-     
     }
-    fecthData()
-  }, [showId])
-  
-  
-    if (showError) {
-      return <div>Something went Wrong {showError.message}</div>
-    }
-    if(showData){
-      return <div>Got Show Data For {showData.name}</div>
-    }
+    fecthData();
+  }, [showId]);
+  return { showData, showError };
+};
+const Show = () => {
+  const { showId } = useParams();
+  const { showData, showError } = useShowbyId(showId);
+  if (showError) {
+    return <div>Something went Wrong {showError.message}</div>;
+  }
+  if (showData) {
+    return <div>Got Show Data For {showData.name}</div>;
+  }
   return <div>Data is loading</div>;
 };
 
